a0929dfd5c11c838db30073e7050deefb71c2911
Rahul SIngh <703rahul.singh@gmail.com>
2022-08-31 02:34:41 +0530

    updated initialize and bet instructions

--- programs/solana-jackpot/src/lib.rs
+++ programs/solana-jackpot/src/lib.rs
@@ -1,5 +1,6 @@
-use anchor_lang::prelude::*;
+// use anchor_lang::prelude::*;
 // use anchor_lang::solana_program::system_program;
+use anchor_lang::{prelude::*, solana_program::system_program};
 
 declare_id!("DP1BGMQdhhTE6CehdvhgQTBZe8mtA4RvmcAg8DQ6oxkd");
 
@@ -7,7 +8,13 @@ declare_id!("DP1BGMQdhhTE6CehdvhgQTBZe8mtA4RvmcAg8DQ6oxkd");
 #[program]
 pub mod solana_jackpot {
     use super::*;
-    const VAULT_PDA_SEED: &[u8] = b"bet";
+
+        use anchor_lang::solana_program::{
+        lamports,
+        program::{invoke, invoke_signed},
+        system_instruction::{transfer , assign_with_seed, assign}
+    };
+
     pub fn initialize_bet(ctx: Context<InitializeBet>, betid: u32) -> Result<()> {
         let betaccount: &mut Account<BetAccount> = &mut ctx.accounts.bet;
 
@@ -15,13 +22,34 @@ pub mod solana_jackpot {
         let bet_id : u32 = betid;
         let mut vec = Vec::new();
         
-        betaccount.betid = bet_id;
-        betaccount.betresult = None;
-        betaccount.bettorlist = vec.clone();
+        betaccount.bet_id = bet_id;
+        betaccount.bet_result = None;
+        betaccount.bettor_list = vec.clone();
         betaccount.timestamp = clock.unix_timestamp;
+        betaccount.vault_pda = *ctx.accounts.vault_pda_account.to_account_info().key;
         // betaccount.betstate = BetState::Started;
-        let (pda, _bump_seed) = Pubkey::find_program_address(&[VAULT_PDA_SEED], ctx.program_id);
-        betaccount.vaultpda = pda;
+
+        Ok(())
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, betid: u32, betAmount: u64) -> Result<()> {
+        let betAccount: &mut Account<BetAccount> = &mut ctx.accounts.bet;
+        let vault_pda_account: &mut Account<BetVaultAccount> = &mut ctx.accounts.vault_pda_account;
+
+        let transfer_instruction = &transfer(
+                        &ctx.accounts.bettor.key,
+                        &vault_pda_account.to_account_info().key,
+                        betAmount,
+                    );
+                    msg!("Paying in {}", betAmount);
+                    invoke(
+                        transfer_instruction,
+                        &[
+                            ctx.accounts.bettor.to_account_info(),
+                            vault_pda_account.to_account_info(),       
+                        ]
+                    );
+
         Ok(())
     }
 }
@@ -29,18 +57,39 @@ pub mod solana_jackpot {
 
 #[derive(Accounts)]
 pub struct InitializeBet<'info> {
-        #[account(init,payer = admin,space = BetAccount::LEN,)]
-        bet: Account<'info, BetAccount>,
+        #[account(init,
+            payer = admin,
+            seeds=[admin.key().as_ref()],
+            bump,
+            space = BetAccount::LEN,)]
+        pub bet: Account<'info, BetAccount>,
+
         #[account(mut)]
-        admin: Signer<'info>, //need to restrict to authorized admin
-        system_program: Program<'info, System>,
+        pub admin: Signer<'info>, //need to restrict to authorized admin
+
+        pub system_program: Program<'info, System>,
+
+        #[account(init,
+        payer = admin,
+        seeds=[b"escrow"],
+        bump,
+        space = BetAccount::LEN,)]
+        pub vault_pda_account: Account<'info, BetVaultAccount>,
 }
 
 
-// #[derive(Accounts)]
-// pub struct PlaceBet<'info> {
+#[derive(Accounts)]
+pub struct PlaceBet<'info> {
+    #[account(mut)]
+    pub bettor: Signer<'info>,
 
-// }
+    pub bet: Account<'info, BetAccount>,
+
+    pub system_program: Program<'info, System>,
+
+    #[account( mut, constraint = bet.vault_pda == *vault_pda_account.to_account_info().key)]
+    pub vault_pda_account: Account<'info, BetVaultAccount>,
+}
 
 // #[derive(Accounts)]
 // pub struct DeclareResult<'info> {
@@ -61,13 +110,16 @@ pub struct InitializeBet<'info> {
 
 #[account]
 pub struct BetAccount {
-    pub betid: u32,
-    pub betresult: Option<u8>,
-    pub bettorlist:Vec<Pubkey>,
+    pub bet_id: u32,
+    pub bet_result: Option<u8>,
+    pub bettor_list:Vec<Pubkey>,
     pub timestamp: i64,
-    pub vaultpda : Pubkey,
+    pub vault_pda : Pubkey,
     // pub betstate: BetState,
 }
+
+#[account]
+pub struct BetVaultAccount {}
 
 // const BET_STATE: usize = 8; //just to be safe
 const BET_RESULT: usize = 8; //just to be safe
@@ -95,3 +147,136 @@ pub enum ErrorCode {
     #[msg("Invalid Bet")]
     InvalidBet,
 }
+
+
+
+// use anchor_lang::{prelude::*, solana_program::system_program};
+
+// declare_id!("E41ZWCPjxsHmAv6DhUdfduj8W2bt7VCnq4RiypAL1RYc");
+
+// #[program]
+// pub mod lock {
+    // use anchor_lang::solana_program::{
+    //     lamports,
+    //     program::{invoke, invoke_signed},
+    //     system_instruction::{transfer , assign_with_seed, assign}
+    // };
+
+//     use super::*;
+//     pub fn initialize(ctx: Context<Initialize>, bump: u8, escrow_bump: u8, authority: Pubkey) -> Result<()> {
+//         let lock_account = &mut ctx.accounts.lock_account;
+//         //let tx  = &assign(lock_account.to_account_info().key, ctx.accounts.owner.to_account_info().key);
+//         lock_account.authority = authority;
+//         lock_account.owner = *ctx.accounts.owner.key;
+//         lock_account.locked = true;
+//         lock_account.bump = bump;
+//         lock_account.escrow_bump = escrow_bump;
+//         lock_account.escrow_pda = *ctx.accounts.lock_escrow_account.to_account_info().key;
+//         Ok(())
+//     }
+//     pub fn unlock(ctx: Context<Unlock>) -> Result<()> {
+//         let lock_account = &mut ctx.accounts.lock_account;
+//         lock_account.locked = false;
+//         Ok(())
+//     }
+//     pub fn lock(ctx: Context<Unlock>) -> Result<()> {
+//         let lock_account = &mut ctx.accounts.lock_account;
+//         lock_account.locked = true;
+//         Ok(())
+//     }
+//     pub fn withdraw(ctx: Context<Withdraw>, lamports: u64) -> Result<()> {
+//         let lock_account = &mut ctx.accounts.lock_account;
+//         let lock_escrow_account = &mut ctx.accounts.lock_escrow_account;
+
+//         **lock_escrow_account.to_account_info().try_borrow_mut_lamports()? -= lamports;
+//         **ctx.accounts.owner.to_account_info().try_borrow_mut_lamports()? += lamports;
+//         Ok(())
+//     }
+
+//     pub fn payin(ctx: Context<Payin>, lamports: u64) -> Result<()> {
+//         let lock_account = &mut ctx.accounts.lock_account;
+//         let lock_escrow_account = &mut ctx.accounts.lock_escrow_account;
+//         let transfer_instruction = &transfer(
+//             &lock_account.owner,
+//             &lock_escrow_account.to_account_info().key,
+//             lamports,
+//         );
+//         msg!("Paying in {}", lamports);
+//         invoke(
+//             transfer_instruction,
+//             &[
+//                 ctx.accounts.owner.to_account_info(),
+//                 lock_escrow_account.to_account_info(),       
+//             ]
+//         );
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// #[instruction(bump: u8, escrow_bump: u8)]
+// pub struct Initialize<'info> {
+    // #[account(init,
+    // payer=owner,
+    // space=8 + 32 + 32 + 1 + 1 + 1 + 32 ,
+    // seeds=[owner.key().as_ref()],
+    // bump)
+    // ]
+//     pub lock_account: Account<'info, LockAccount>,
+//     #[account(mut)]
+//     pub owner: Signer<'info>,
+//     pub system_program: Program<'info, System>,
+//     #[account(init,
+//         payer=owner,
+//         space=8,
+//         seeds=[owner.key().as_ref(),b"escrow"],
+//         bump)
+//         ]
+//     pub lock_escrow_account: Account<'info, LockEscrowAccount>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Unlock<'info> {
+//     #[account(mut, has_one = authority)]
+//     pub lock_account: Account<'info, LockAccount>,
+//     #[account(signer)]
+//     pub authority: AccountInfo<'info>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Withdraw<'info> {
+//     #[account(mut,has_one=owner, constraint = !lock_account.locked, close=owner)]
+//     pub lock_account: Account<'info, LockAccount>,
+//     #[account(mut, signer)]
+//     pub owner: AccountInfo<'info>,
+//     pub system_program: Program<'info, System>,
+//     #[account( mut, constraint = lock_account.escrow_pda == *lock_escrow_account.to_account_info().key)]
+//     //#[account( mut)]
+//     pub lock_escrow_account: Account<'info, LockEscrowAccount>
+// }
+
+// #[derive(Accounts)]
+// pub struct Payin<'info> {
+//     #[account(has_one = owner)]
+//     pub lock_account: Account<'info, LockAccount>,
+//     #[account(signer)]
+//     pub owner: AccountInfo<'info>,
+//     pub system_program: Program<'info, System>,
+//     #[account( mut, constraint = lock_account.escrow_pda == *lock_escrow_account.to_account_info().key)]
+//     pub lock_escrow_account: Account<'info, LockEscrowAccount>
+// }
+
+
+// #[account]
+// pub struct LockAccount {
+//     pub owner: Pubkey,
+//     pub authority: Pubkey,
+//     pub locked: bool,
+//     pub bump: u8,
+//     pub escrow_bump: u8,
+//     pub escrow_pda: Pubkey
+// }
+
+
+// #[account]
+// pub struct LockEscrowAccount {}
